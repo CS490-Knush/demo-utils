779d806fa834d8ebb9bf5385214890cf3ddceb96
root <root@large-instance-ubuntu.us-east1-b.c.cpsc490-222104.internal>
2018-12-17 05:17:48 +0000

    scripts for top20-25

---
+++ scripts/gen_set_bw_script/gen_set_bw_script_top20-25.py
@@ -0,0 +1,44 @@
+bw = {
+        # hosts
+        "1:1": "25000",
+        "3:1": "25000",
+        "6:1": "25000",
+        "8:1": "25000",
+
+        # top
+        "1:2": "20000",
+        "2:1": "20000",
+        "2:2": "20000",
+        "3:2": "20000",
+
+        # bottom
+        "6:3": "20000",
+        "7:1": "20000",
+        "7:2": "20000",
+        "8:2": "20000",
+
+        # X
+        "1:3": "25000",
+        "3:3": "25000",
+        "4:1": "25000",
+        "4:2": "25000",
+        "5:1": "25000",
+        "5:2": "25000",
+        "6:2": "25000",
+        "8:3": "25000",
+
+        # shared
+        "4:3": "25000",
+        "5:3": "25000"
+}
+
+for ports, capacity in bw.items():
+	curl  = "curl -X PUT -u admin:admin " + \
+	"http://172.17.0.2:8181/restconf/config/alto-bwmonitor:config-capacity/capacity/openflow:%s " + \
+	"-H 'Content-type: application/json' " + \
+	"-d '{\"capacity\": {\"port-id\":\"openflow:%s\", " + \
+	"\"capacity\": %s}}'"
+	
+	print(curl % (ports, ports, capacity))
+
+
